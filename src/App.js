a8b22cc35a3951a051c2b544ced2dae90c598e87
Emeli Gabriela Casemiro Aderaldo <emeliaderaldo@alu.ufc.br>
2023-05-15 15:43:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 
-import Pokemon from './prova/questao2';
-// import Questao01 from './prova/questao1';
+// import Pokemon from './prova/questao2';
+// import Questao01X from './prova/questao1';
+import Populacao from './prova/questao3';
 
 function App() {
   return (
     <div >
-      {/* <Questao01 />  */}
-      <Pokemon />    
+      {/* <Questao01X /> 
+      <Pokemon />     */}
+      <Populacao /> 
+
     </div>
   );
 }
